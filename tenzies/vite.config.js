88f0e4383d4aedf34b033057e6714bfe05719347
satheesh1022005 <satheesh1022005@gmail.com>
2024-07-12 12:18:45 +0530

    trying to fix blanck error

--- tenzies/vite.config.js
+++ tenzies/vite.config.js
@@ -4,9 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "./", // Ensure relative paths
   build: {
-    rollupOptions: {
-      external: ["react-router-dom"],
-    },
+    // No external options here
   },
 });
